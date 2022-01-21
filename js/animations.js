3d498268b12b0d88f43e6c9d4d6f68904097a949
DmitriVic <DmitriVic>
2022-01-21 11:49:44 +0300

    21.01.22

--- js/animations.js
+++ js/animations.js
@@ -21,3 +21,37 @@ function increaseNumberAnimationStep (i, element, endNumber) {
  //document.getElementsByClassName('features__clients-count')
  //increaseNumberAnimationStep
 
+ document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+	if (event.target.value === 'other') {
+		// Должны добавить еще одно текстовое поле
+		const formContainer = document.createElement('div')
+		formContainer.classList.add("form__group", "form__other")
+
+		const input = document.createElement('input')
+		input.setAttribute('placeholder', 'Введите ваш вариант');
+		input.setAttribute('type', 'text');
+
+		formContainer.appendChild(input) 
+		document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+	 }
+  
+	 if (event.target.value !== 'other') {
+		// Удаляем ранее добавленное текстовое поле, если оно есть в DOM
+	 }
+	
+ });
+
+
+ function updateScroll() {
+	let header = document.querySelector('header')
+	if (window.scrollY > 0) {
+		header.classList.add('header__scrolled');
+	 } else {
+		header.classList.remove("header__scrolled");
+	 }
+ }
+
+ window.addEventListener("scroll", updateScroll);
+//let f = document.querySelector("#form")
+
+//document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit'));
